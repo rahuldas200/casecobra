37e01ef8bc0735a0ec2dde4811a2fc2d16fc7c7a
Rahul Das <rdas53300@gmail.com>
2024-07-14 13:44:25 +0530

    strip_success_url_fixed

--- src/app/configure/preview/action.ts
+++ src/app/configure/preview/action.ts
@@ -68,7 +68,7 @@ export const createCheckoutSession = async ({configId}:{configId:string}) =>{
     })
 
     const stripSession = await stripe.checkout.sessions.create({
-        success_url:`${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+        success_url:`${process.env.NEXT_PUBLIC_SERVER_URL}/api/thank-you?orderId=${order.id}`,
         cancel_url:`${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
         payment_method_types:['card'],
         mode:"payment",
